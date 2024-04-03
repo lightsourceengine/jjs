0fc3186a2329ee9f97faa85cbe15a84fc2da1c48
Daniel Anderson <804021+dananderson@users.noreply.github.com>
2024-04-03 07:17:00 -0700

    Ensure target specific flags are added to buildoptions and amalgam tests

--- cmake/zig-macos-aarch64.cmake
+++ cmake/zig-macos-aarch64.cmake
@@ -3,5 +3,4 @@ include ("${CMAKE_CURRENT_LIST_DIR}/zig.cmake")
 set(CMAKE_SYSTEM_NAME Darwin)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-set(CMAKE_C_FLAGS "-target aarch64-macos-none")
-set(FLAGS_COMMON_ARCH "${CMAKE_C_FLAGS}")
+set(CMAKE_C_FLAGS_INIT "-target aarch64-macos-none")
